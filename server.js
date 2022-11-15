f327331a775fc4fec26b039e40bbc9d93ea7354b
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-11-15 12:24:57 +0200

    fix: DPO server

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const apiKey = require("./app/config/loancalculatorapikey.js")
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require("axios");
-const path = require('path')
-const {sendSMS} = require("./functions");
-const calculateLoanUrl = require('./app/config/calculate-loan-url.js');
 require('dotenv').config()
 
 
@@ -19,37 +15,61 @@ app.use(bodyParser.urlencoded({extended: true}));
 // cross origin access to allow backend to communicate with frontend
 app.use(cors());
 
-const db = require("./app/models/index");
-// to drop existing database and resync the database
-db.sequelize.sync({force: false})
-
-require("./app/routes/loan_application.routes")(app);
-
-
-app.use(express.static(path.resolve(__dirname, 'assets')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'assets', 'index.html'));
-});
-
 /**
- * Post route for calculating a loan's payment options
- * @params formData object containing information required to calculate the loan
- * @return loanOptions object with array or loan payment options
+ * Post route for making a payment
+ * @params formData object containing information required to make a payment
+ * @return response object with array or payment options
  */
-app.post('/calculateInternal', async (req,
-                                  res) => {
+app.post('/makePayment', async (req, res) => {
     try {
         // get form data from the body
-        const formData = req.body.data;
+        const formData = req.body;
+        console.log(req.body)
 
+        const xmlBodyStr = `
+        <?xml version="1.0" encoding="utf-8"?>
+            <API3G>
+                <CompanyToken>${process.env.COMPANY_TOKEN}</CompanyToken>
+                <Request>createToken</Request>
+                <Transaction>
+                    <PaymentAmount>${formData.amount}</PaymentAmount>
+                    <PaymentCurrency>${formData.currency || 'Bwp'}</PaymentCurrency>
+                    <PTL>${process.env.PTL}</PTL>
+                    <PTLtype>${process.env.PTL_TYPE}</PTLtype>
+                    <customerEmail>${formData.customerEmail}</customerEmail>
+                    <customerFirstName>${formData.customerFirstName}</customerFirstName>
+                    <customerLastName>${formData.customerLastName}</customerLastName>
+                    <customerAddress>${formData.customerAddress}</customerAddress>
+                    <customerCity>${formData.customerCity}</customerCity>
+                    <customerCountry>${formData.customerCountry}</customerCountry>
+                    <customerDialCode>${formData.customerDialCode}</customerDialCode>
+                    <customerPhone>${formData.customerPhone}</customerPhone>
+                    <CompanyAccRef>${formData.CompanyAccRef}</CompanyAccRef>
+                </Transaction>
+                <Services>
+                    <Service>
+                        <ServiceType>${process.env.SERVICE_TYPE}</ServiceType>
+                        <ServiceDescription>${formData.serviceDiscription}</ServiceDescription>
+                        <ServiceDate>${formData.serviceDate}</ServiceDate>
+                    </Service>
+                </Services>
+            </API3G>`;
+        let config = {
+            headers: {
+                'Content-Type': 'text/xml',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
+
+        };
         // call api and supply data provided
         // console.log("calculateLoanUrl:",calculateLoanUrl,"formData:", formData, "apiKey:",apiKey)
-        const response = await axios.post(calculateLoanUrl, formData, apiKey);
+        const response = await axios.post("https://secure.3gdirectpay.com/API/v6/", xmlBodyStr, config).then(async response => {
+            return response
+        })
         // get the data if successful
-        const data = {loanOptions: JSON.parse(response.data).section};
-
-        res.status(200).send(data);
+        res.status(200).send(response.data);
     } catch (err) {
         // console.log(err);
         // return any error info with 500 status code
@@ -59,38 +79,45 @@ app.post('/calculateInternal', async (req,
 })
 
 /**
- * Post route for sending an SMS to a phone number
- * @params params object: {params : { recipient: '2677123456', message: 'Hello World!' } }
- * @return loanOptions object with array or loan payment options
+ * Post route for making a payment
+ * @params formData object containing information required to make a payment
+ * @return response object with array or payment options
  */
-app.post('/sendSMS', async (req,
-                            res) => {
+app.post('/verifyPayment', async (req, res) => {
     try {
-        // get params from body
+        // get form data from the body
+        const formData = req.body;
 
-        const {recipient, message} = req.body.data;
+        const xmlBodyStr = `<?xml version="1.0" encoding="utf-8"?>
+        <API3G>
+            <CompanyToken>${process.env.COMPANY_TOKEN}</CompanyToken>
+            <Request>verifyToken</Request>
+            <TransactionToken>${formData.TransactionToken}</TransactionToken>
+        </API3G>`;
+        let config = {
+            headers: {
+                'Content-Type': 'text/xml',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
 
-        // check if both are present
-        if (!(recipient && message)) {
-            // one or both are missing, send error message to client
-            res.status(400).send('Missing parameter(s). ' +
-                'Required {"params" : { "recipient": "2677123456", ' +
-                '"message": "Hello World!" } }');
-        }else{
-            // call sendSMS function with the parameters
-            const responseData = await sendSMS(recipient, message);
-
-            // return response from message API
-            res.status(200).send(responseData);
-        }
-
-
+        };
+        // call api and supply data provided
+        // console.log("calculateLoanUrl:",calculateLoanUrl,"formData:", formData, "apiKey:",apiKey)
+        const response = await axios.post("https://secure.3gdirectpay.com/API/v6/", xmlBodyStr, config).then(async response => {
+            return response
+        })
+        // get the data if successful
+        res.status(200).send(response.data);
     } catch (err) {
-        // for other errors, send error message
+        // console.log(err);
+        // return any error info with 500 status code
         console.log(err.message)
-        res.status(500).send(err.message);
+        res.status(500).send(err.message)
     }
 })
+
 
 // listen at endpoint
 const PORT = process.env.PORT || 5000;
